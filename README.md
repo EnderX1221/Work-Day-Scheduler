e97298e9543d489f5c4a765fb3da56640f401082
Javier Razo <javier.razo1221@gmail.com>
2022-03-13 22:39:58 -0500

    finished the JAva

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ## Purpose
 A Work Day Scheduler that allows you to save planned activities
 
-
+"C:\Users\Ender\OneDrive\Desktop\Screenshot 2022-03-13 223648.jpg"
 
 ## Built With
 * HTML
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,17 @@
 var tasks = () ;
 
 var createTask = funtion(taskText, taskDate, taskList) {
+//Create Tasks
+    var taskLi = $("<li>").addClass("list-item");
+   .addClass("badge, badge-primary,badge-info") 
+   .text(taskDate);
 
+   taskLi.append(taskText,taskDate)
+
+   $("#list-"= taskList).append(taskLi);
+
+   var logInfo = () ;
+//Log the tasks
+   var storeInfo =function(toDo,inProcess, underReview, inputDone)
 }
+tasks();
